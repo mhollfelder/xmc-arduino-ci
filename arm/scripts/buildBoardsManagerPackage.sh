6c408ff22635ebeaaae4900954011dd2e61a3f6e
Manuel Hollfelder <manuel.hollfelder@infineon.com>
2020-02-08 01:30:14 +0100

    Update buildBoardsManagerPackage.sh

--- arm/scripts/buildBoardsManagerPackage.sh
+++ arm/scripts/buildBoardsManagerPackage.sh
@@ -72,9 +72,9 @@ echo "Package: $PKG_URL"
 
 pushd ..
 # Create directory for the package
-outdir=package/versions/$visiblever/$package_name
 srcdir=$PWD
-rm -rf package/versions/$visiblever
+outdir=$srcdir/package/versions/$visiblever/$package_name
+rm -rf $srcdir/package/versions/$visiblever
 mkdir -p $outdir
 
 # Some files should be excluded from the package
@@ -210,98 +210,9 @@ asset_uri=${asset_uri//\{?name,label\}/}
 echo "Asset URI is $asset_uri"
 
 echo "Uploading the package"
-curl --silent "${curl_gh_token_arg[@]}" -H "Content-Type: application/octet-stream" --data-binary "$package_name.zip" "${asset_uri}?name=$package_name.zip"
+curl --silent "${curl_gh_token_arg[@]}" -H "Content-Type: application/zip" --data-binary "$package_name.zip" "${asset_uri}?name=$package_name.zip"
 
 popd
 popd
 
 echo "All done for nightly build"
-
-
-# # If normal
-
-# echo "Making package_infineon_index.json"
-
-# jq_arg=".packages[0].platforms[0].version = \"$visiblever\" | \
-#     .packages[0].platforms[0].url = \"$PKG_URL\" |\
-#     .packages[0].platforms[0].archiveFileName = \"$package_name.zip\""
-
-# if [ -z "$is_nightly" ]; then
-#     jq_arg="$jq_arg |\
-#         .packages[0].platforms[0].size = \"$size\" |\
-#         .packages[0].platforms[0].checksum = \"SHA-256:$sha\""
-# fi
-
-# cat $srcdir/scripts/package_infineon_index.template.json | \
-#     jq "$jq_arg" > package_infineon_index.json
-
-
-# curl_gh_token_arg=()
-# if [ ! -z "$CI_GITHUB_API_ENVIRONMENT" ]; then
-#     curl_gh_token_arg=(-H "Authorization: token $CI_GITHUB_API_ENVIRONMENT")
-#     echo "Here1"
-# fi
-
-# echo "${curl_gh_token_arg[@]}"
-
-# # Get previous release name
-# curl --silent "${curl_gh_token_arg[@]}" "$REPO_API_URL" > releases.json
-# cat releases.json
-
-# # Previous final release (prerelase == false)
-# prev_release=$(jq -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[0].tag_name' releases.json)
-# # Previous release (possibly a pre-release)
-# prev_any_release=$(jq -r '. | map(select(.draft == false)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' releases.json)
-# # Previous pre-release
-# prev_pre_release=$(jq -r '. | map(select(.draft == false and .prerelease == true)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' releases.json)
-
-# echo "Previous release: $prev_release"
-# echo "Previous (pre-?)release: $prev_any_release"
-# echo "Previous pre-release: $prev_pre_release"
-
-# # Make all released versions available in one package (i.e. don't separate stable/staging versions)
-# base_ver=$prev_any_release
-
-# # Download previous release
-# echo "Downloading base package: $base_ver"
-# old_json=package_infineon_index_stable.json
-# curl -L -o $old_json "$JSON_URI_REPO/${base_ver}/package_infineon_index.json"
-# new_json=package_infineon_index.json
-
-# set +e
-# # Merge the old and new, then drop any obsolete package versions
-# python3 ../../../scripts/merge_packages.py $new_json $old_json >tmp && mv tmp $new_json && rm $old_json
-
-# # Verify the JSON file can be read, fail if it's not OK
-# set -e
-# cat $new_json | jq empty
-
-# # Create the release and push it to GitHub
-# generate_post_data()
-# {
-#   cat <<EOF
-# {
-#   "tag_name": "${visiblever}",
-#   "target_commitish": "master",
-#   "name": "Release of version ${visiblever}",
-#   "body": "Description of the release",
-#   "draft": false,
-#   "prerelease": false
-# }
-# EOF
-# }
-
-# echo "${curl_gh_token_arg[@]}"
-# echo --data "$(generate_post_data)"
-
-
-# echo "Creating the new release"
-# curl --silent "${curl_gh_token_arg[@]}" --data "$(generate_post_data)" "$REPO_API_URL"
-
-# echo "Uploading the package"
-# curl --silent "${curl_gh_token_arg[@]}" --data-binary "$package_name.zip" -H "Content-Type: application/octet-stream" "$REPO_UPLOAD_URL/${visiblever}/assets?name=$package_name.zip"
-
-# popd
-# popd
-
-# echo "All done"
