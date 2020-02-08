15ff3905cb93ea288577b80cda181b6c835af2ea
Manuel Hollfelder <manuel.hollfelder@infineon.com>
2020-02-08 02:29:16 +0100

    Update buildBoardsManagerPackage.sh

--- arm/scripts/buildBoardsManagerPackage.sh
+++ arm/scripts/buildBoardsManagerPackage.sh
@@ -105,7 +105,7 @@ echo "Making $package_name.zip"
 ls ./
 echo $package_name
 #zip -qr $package_name.zip $package_name
-zip -qr $package_name.zip ./
+zip -qr $package_name.zip ./../..
 rm -rf $package_name
 
 # Calculate SHA sum and size
